b5a0ae5f87809ec138ae8cb9415ad2f63bdbcd24
haarer <haarer@users.noreply.github.com>
2021-05-30 21:30:48 +0200

    add crc32 from binascii/zlib, and passing crc width

--- identifychecksum.py
+++ identifychecksum.py
@@ -2,7 +2,7 @@
 #identifychecksum.py
 #
 # Usage: 
-#   identifychecksum.py <filename> <known checksum>
+#   identifychecksum.py <filename> <known checksum> <checksum bit length>
 #
 # Description:
 #   The Script computes sums of by known checksum algorithms and tries
@@ -19,40 +19,79 @@
 
 
 from mmap import ACCESS_READ, mmap
-import crccheck,sys
+import crccheck,sys,re
 
-if len(sys.argv) != 3:
-  print ("need file and checksum as arguments")
+print(len(sys.argv))
+if len(sys.argv) < 3 or len(sys.argv) > 4:
+  print ("need file, checksum and optional checksum bit length as arguments")
   sys.exit(-1)
 
-sumresults={}
-with open(sys.argv[1], 'rb') as fh, mmap(fh.fileno(), 0, access=ACCESS_READ) as mm:
-  for clsname in dir(crccheck.crc):
-    try:
-      cc = getattr(crccheck.crc,clsname) 
-      res = cc.calc((b[0] for b in mm))
-      if res in sumresults:
-        sumresults[res].add(clsname)
-      else:
-        sumresults[res]=[clsname]
-      print("crc: {0}  = {1:x}".format(clsname,res) )
-    except:
-      pass
+checksumtoidentify= int(sys.argv[2],16)
 
-  for clsname in dir(crccheck.checksum):
-    try:
-      cc = getattr(crccheck.checksum,clsname) 
-      res = cc.calc((b[0] for b in mm))
-      if res in sumresults:
-        sumresults[res].add(clsname)
-      else:
+if len(sys.argv) ==4:
+    checksumlength= int(sys.argv[3])
+else:
+    checksumlength=None
+
+sumresults={}
+
+
+with open(sys.argv[1], 'rb') as fh:
+  data=fh.read()
+  print("file read")
+
+#tests with binascii
+  if checksumlength == 32 or checksumlength == None:
+    print("testing crc32 from binasci (same as zlib)")
+    import binascii
+    res = binascii.crc32(data) & 0xFFFFFFFF
+    clsname = "crc32"
+    if res in sumresults:
+        sumresults[res].extend(clsname)
+    else:
         sumresults[res]=[clsname]
-      print("crc: {0}  = {1:x}".format(clsname,res) )
-    except:
-      pass
+    print("crc: {0}  = {1:x} ({2} bit width)".format(clsname,res,32) )
+
+
+# tests with crc module - has an identify but it does basically the same stuff we do
+
+
+  print("testing crccheck.crc {0} bit functions".format(checksumlength))
+  for cls in crccheck.crc.ALLCRCCLASSES:
+    clsname = cls.__name__
+    if checksumlength == None or cls._width == checksumlength:
+      try:
+        res = cls.calc(data)
+        if res in sumresults:
+          sumresults[res].extend(clsname)
+        else:
+          sumresults[res]=[clsname]
+        print("crc: {0}  = {1:x} ({2} bit width)".format(clsname,res,cls._width) )
+      except:
+        pass
+
+
+
+
+# tests with checksum module
+  print("testing crccheck.checksum {0} bit functions".format(checksumlength))
+  for cls in crccheck.checksum.ALLCHECKSUMCLASSES:
+    clsname = cls.__name__
+    if checksumlength == None or cls._width == checksumlength:
+      try:
+        res = cls.calc(data)
+        if res in sumresults:
+          sumresults[res].extend(clsname)
+        else:
+          sumresults[res]=[clsname]
+        print("crc: {0}  = {1:x} ({2} bit width)".format(clsname,res,cls._width) )
+      except:
+        pass
+
+
 
 #print(sumresults)
-checksumtoidentify= int(sys.argv[2],16)
+
 #print(sumresults[checksumtoidentify])
 try:
   print("{0:x} is a {1}".format(checksumtoidentify,sumresults[checksumtoidentify]))
